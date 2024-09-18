02bdb92d4bebc39f8a141548e5b1bf0d2084cd9b
User <ksr005@stud-r1>
2024-09-18 10:34:28 +0300

    ihi

--- super_matrix.c
+++ super_matrix.c
@@ -4,9 +4,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define SIZE 3  // Размер матриц (например, 3x3)
+#define SIZE 3  
 
-// Функция для вывода матрицы
 void print_matrix(int (*matrix)[SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -16,7 +15,7 @@ void print_matrix(int (*matrix)[SIZE]) {
     }
 }
 
-// Функция для умножения строки матрицы A на матрицу B и записи результата в матрицу C
+
 void multiply_row(int (*A)[SIZE], int (*B)[SIZE], int (*C)[SIZE], int row) {
     for (int j = 0; j < SIZE; j++) {
         C[row][j] = 0;
@@ -27,9 +26,8 @@ void multiply_row(int (*A)[SIZE], int (*B)[SIZE], int (*C)[SIZE], int row) {
 }
 
 int main() {
-    int (*A)[SIZE], (*B)[SIZE], (*C)[SIZE];  // Указатели на матрицы A, B, C
-
-    // Выделение разделяемой памяти для всех матриц
+    int (*A)[SIZE], (*B)[SIZE], (*C)[SIZE];  
+    
     A = mmap(NULL, SIZE * SIZE * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     B = mmap(NULL, SIZE * SIZE * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     C = mmap(NULL, SIZE * SIZE * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -39,7 +37,7 @@ int main() {
         exit(1);
     }
 
-    // Инициализация матриц A и B
+    
     printf("Введите элементы матрицы A (%dx%d):\n", SIZE, SIZE);
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -54,39 +52,33 @@ int main() {
         }
     }
 
-    // Вывод матриц A и B
     printf("Матрица A:\n");
     print_matrix(A);
 
     printf("Матрица B:\n");
     print_matrix(B);
 
-    // Создание дочерних процессов для вычисления строк результирующей матрицы C
     for (int i = 0; i < SIZE; i++) {
         pid_t pid = fork();
         if (pid == 0) {
-            // Дочерний процесс вычисляет i-ю строку матрицы C
             multiply_row(A, B, C, i);
             munmap(A, SIZE * SIZE * sizeof(int));
             munmap(B, SIZE * SIZE * sizeof(int));
             munmap(C, SIZE * SIZE * sizeof(int));
-            exit(0);  // Завершаем дочерний процесс
+            exit(0);  
         } else if (pid < 0) {
             perror("fork");
             exit(1);
         }
     }
 
-    // Ожидание завершения всех дочерних процессов
     for (int i = 0; i < SIZE; i++) {
         wait(NULL);
     }
 
-    // Вывод результирующей матрицы C
     printf("Результирующая матрица C (A x B):\n");
     print_matrix(C);
 
-    // Освобождение разделяемой памяти
     munmap(A, SIZE * SIZE * sizeof(int));
     munmap(B, SIZE * SIZE * sizeof(int));
     munmap(C, SIZE * SIZE * sizeof(int));
